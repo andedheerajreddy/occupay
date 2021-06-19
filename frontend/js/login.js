14904c8640767f0c72a8922da59777d41b47596b
Dheeraj  Reddy <adreddy.jmd@gmail.com>
2021-06-19 10:59:30 +0530

    login signup validation

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,3 +1,10 @@
+function snackbar(mssg) {
+    var x = document.getElementById("snackbar");
+    x.innerHTML = `<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ${mssg}`
+    x.className = "show";
+    setTimeout(function() { x.className = x.className.replace("show", ""); }, 2000);
+}
+
 function login() {
     console.log("Hello");
     //Email verification
@@ -7,105 +14,39 @@ function login() {
         else return true;
     }
 
-    var name = String(document.getElementsByClassName("register")[0].value);
-    var email = String(document.getElementsByClassName("register")[1].value);
-    var password = String(document.getElementsByClassName("register")[2].value);
-    var dateOfBirth = String(document.getElementsByClassName("register")[3].value);
-    var Street = String(document.getElementsByClassName("register")[4].value);
-    var City = String(document.getElementsByClassName("register")[5].value);
-    var pincode = String(document.getElementsByClassName("register")[6].value);
-    var state = String(document.getElementsByClassName("register")[7].value);
-    var country = String(document.getElementsByClassName("register")[8].value);
-    //var mobileNumber = String(document.getElementsByClassName("register")[9].value);
+    var email = String(document.getElementsByClassName("login")[0].value);
+    var password = String(document.getElementsByClassName("login")[1].value);
 
-    var c = 9;
+    var c = 2;
     if (email == "") {
-    //     document.getElementById("alertmsg").innerHTML = ` <div class="alert alert-danger alert-dismissible">
-    //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    //     <strong>Sorry!</strong>Enter the Email
-    //   </div>`;
+        snackbar("Enter email");
+        c--;
+    } else if (password == "") {
+        snackbar("Enter password");
+        c--;
+    } else if (!IsEmail(email)) {
+        snackbar("Enter valid email");
         c--;
     }
-    if(name==""){c--;}
-    if(dateOfBirth==""){c--;}
-    if(Street==""){c--;}
-    if(City==""){c--;}
-    if(pincode==""){c--;}
-    if(state==""){c--;}
-    if(country==""){c--;}
-    //if(mobileNumber==""){c--;}
-
-
-
-
-
-   else if (password == "") {
-    //     document.getElementById("alertmsg").innerHTML = ` <div class="alert alert-danger alert-dismissible">
-    //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    //     <strong>Sorry!</strong>Enter the password
-    //   </div>`;
-        c--;
-    } 
-    //else document.getElementById("alertmsg").innerHTML = ``;
-
-    if (c == 10) {
-        if (!IsEmail(emailid)) {
-        //     document.getElementById("alertmsg").innerHTML = ` <div class="alert alert-danger alert-dismissible">
-        //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-        //     <strong>Sorry!</strong>invalid Email
-        //   </div>`;
-            c--;
-        }
-    }
-
-    console.log(c);
-    //ajax call to create an instance to the user in database
-    //alert("sidfoisd");
-    if (c == 9) {
+    if (c == 2) {
         //alert("Ajax");
         $.ajax({
             type: "POST",
-            url: "/api/user/signup",
+            url: "/api/user/login",
             data: {
                 email: email,
                 password: password,
-                name:name,
-                dateOfBirth:dateOfBirth,
-                address: {
-                    Street: Street,
-                    City: City,
-                    pincode: pincode,
-                    state: state,
-                    country: country,
-                },
-                mobileNumber:"9849123123"
-
             },
             success: function(resultData) {
-                console.log(resultData)
-                if (resultData.message == "Email already exists")
-                //     document.getElementById("alert").innerHTML = `<div class="alert alert-danger alert-dismissible">
-                //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                //     <strong>Sorry!</strong>Enter the Email
-                //   </div>`;
-                if (resultData.message == "user created") {
-                    //window.location.href = '/verify';
+                if (resultData.message == "Auth successful") {
+                    localStorage.setItem("userDetails", JSON.stringify(resultData.userDetails));
+                    localStorage.setItem("token", resultData.token);
+                    window.location.href = "/dashboard"
                 }
             }, //sucess
             error: function(resultData) {
-                    if (resultData.responseJSON.message == "Unauthorized access") {
-                        location.href = "/"
-                    } else {
-                    //     var x = document.getElementById("alert");
-
-                    //     x.innerHTML = `<div class="alert alert-danger alert-dismissible">
-                    //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    //     <strong>Sorry!</strong>Enter the Email
-                    //    ${resultData.responseJSON.message}</div>`
-                    //     x.className = "show";
-                    //     setTimeout(function() { x.className = x.className.replace("show", ""); }, 3000);
-                    }
-                } //error
+                snackbar("INVALID CREDENTIALS!!")
+            }
         });
     }
 } //End of signup function
--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,12 +1,19 @@
+function snackbar(mssg) {
+    var x = document.getElementById("snackbar");
+    x.innerHTML = `<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ${mssg}`
+    x.className = "show";
+    setTimeout(function() { x.className = x.className.replace("show", ""); }, 2000);
+}
+
+
 function signup() {
-    console.log("Hello");
+    // console.log("Hello");
     //Email verification
     function IsEmail(email) {
         var regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
         if (!regex.test(email)) return false;
         else return true;
     }
-
     var name = String(document.getElementsByClassName("register")[0].value);
     var email = String(document.getElementsByClassName("register")[1].value);
     var password = String(document.getElementsByClassName("register")[2].value);
@@ -16,47 +23,58 @@ function signup() {
     var pincode = String(document.getElementsByClassName("register")[6].value);
     var state = String(document.getElementsByClassName("register")[7].value);
     var country = String(document.getElementsByClassName("register")[8].value);
-    //var mobileNumber = String(document.getElementsByClassName("register")[9].value);
+    var mobileNumber = String(document.getElementsByClassName("number")[0].value);
 
-    var c = 9;
-    if (email == "") {
-        //     document.getElementById("alertmsg").innerHTML = ` <div class="alert alert-danger alert-dismissible">
-        //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-        //     <strong>Sorry!</strong>Enter the Email
-        //   </div>`;
+    var c = 10;
+    if (name == "") {
         c--;
-    }
-    if (name == "") { c--; }
-    if (dateOfBirth == "") { c--; }
-    if (Street == "") { c--; }
-    if (City == "") { c--; }
-    if (pincode == "") { c--; }
-    if (state == "") { c--; }
-    if (country == "") { c--; }
-    //if(mobileNumber==""){c--;}
-    else if (password == "") {
+        snackbar("Enter name");
+    } else if (email == "") {
+        snackbar("Enter email");
+        c--;
+    } else if (mobileNumber == "") {
+        c--;
+        snackbar("Enter mobile number")
+    } else if (password == "") {
         //     document.getElementById("alertmsg").innerHTML = ` <div class="alert alert-danger alert-dismissible">
         //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         //     <strong>Sorry!</strong>Enter the password
         //   </div>`;
+        snackbar("Enter password")
         c--;
+    } else if (dateOfBirth == "") {
+        c--;
+        snackbar("Enter DOB");
+    } else if (Street == "") {
+        c--;
+        snackbar("Enter Street");
+    } else if (City == "") {
+        c--;
+        snackbar("Select your City");
+    } else if (pincode == "") {
+        c--;
+        snackbar("Enter Pincode")
+    } else if (state == "") {
+        c--;
+        snackbar("Select your State")
+    } else if (country == "") {
+        c--;
+        snackbar("Select your country")
     }
     //else document.getElementById("alertmsg").innerHTML = ``;
-
-    if (c == 10) {
-        if (!IsEmail(emailid)) {
-            //     document.getElementById("alertmsg").innerHTML = ` <div class="alert alert-danger alert-dismissible">
-            //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            //     <strong>Sorry!</strong>invalid Email
-            //   </div>`;
-            c--;
-        }
+    else if (!IsEmail(email)) {
+        //     document.getElementById("alertmsg").innerHTML = ` <div class="alert alert-danger alert-dismissible">
+        //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        //     <strong>Sorry!</strong>invalid Email
+        //   </div>`;
+        snackbar("Enter valid email")
+        c--;
     }
 
     console.log(c);
     //ajax call to create an instance to the user in database
     //alert("sidfoisd");
-    if (c == 9) {
+    if (c == 10) {
         //alert("Ajax");
         $.ajax({
             type: "POST",
@@ -73,32 +91,23 @@ function signup() {
                     state: state,
                     country: country,
                 },
-                mobileNumber: "9849123123"
+                mobileNumber: mobileNumber
 
             },
             success: function(resultData) {
-                console.log(resultData)
-                if (resultData.message == "Email already exists")
-                //     document.getElementById("alert").innerHTML = `<div class="alert alert-danger alert-dismissible">
-                //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                //     <strong>Sorry!</strong>Enter the Email
-                //   </div>`;
-                    if (resultData.message == "user created") {
-                    //window.location.href = '/verify';
+                if (resultData.message == "user created") {
+                    window.location.href = '/login'
                 }
             }, //sucess
             error: function(resultData) {
-                    if (resultData.responseJSON.message == "Unauthorized access") {
-                        location.href = "/"
-                    } else {
-                        //     var x = document.getElementById("alert");
 
-                        //     x.innerHTML = `<div class="alert alert-danger alert-dismissible">
-                        //     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                        //     <strong>Sorry!</strong>Enter the Email
-                        //    ${resultData.responseJSON.message}</div>`
-                        //     x.className = "show";
-                        //     setTimeout(function() { x.className = x.className.replace("show", ""); }, 3000);
+                    if (resultData.responseJSON.message == "Email already exists") {
+                        var x = document.getElementById("snackbar");
+                        x.innerHTML = `<i class="fa fa-exclamation-circle" aria-hidden="true"></i>Email already exists`
+                        x.className = "show";
+                        setTimeout(function() { x.className = x.className.replace("show", ""); }, 3000);
+                    } else {
+                        window.location.href = '/register'
                     }
                 } //error
         });
